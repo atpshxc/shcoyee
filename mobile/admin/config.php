3e0dd0b7840cc058d49f6ae7e2b9ea1557d62ed8
atpshxc <emeson_ch@126.com>
2015-08-31 17:46:22 +0800

    fix some issue

--- mobile/admin/config.php
+++ mobile/admin/config.php
@@ -1,25 +1,25 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/shcoyee/admin/');
-define('HTTP_CATALOG', 'http://localhost/shcoyee/');
-define('HTTP_IMAGE', 'http://localhost/shcoyee/image/');
+define('HTTP_SERVER', 'http://localhost/shcoyee/mobile/admin/');
+define('HTTP_CATALOG', 'http://localhost/shcoyee/mobile/');
+define('HTTP_IMAGE', 'http://localhost/shcoyee/mobile/image/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/shcoyee/admin/');
-define('HTTPS_IMAGE', 'http://localhost/shcoyee/image/');
+define('HTTPS_SERVER', 'http://localhost/shcoyee/mobile/admin/');
+define('HTTPS_IMAGE', 'http://localhost/shcoyee/mobile/image/');
 
 // DIR
-define('DIR_APPLICATION', 'D:\wamp\www\shcoyee/admin/');
-define('DIR_SYSTEM', 'D:\wamp\www\shcoyee/system/');
-define('DIR_DATABASE', 'D:\wamp\www\shcoyee/system/database/');
-define('DIR_LANGUAGE', 'D:\wamp\www\shcoyee/admin/language/');
-define('DIR_TEMPLATE', 'D:\wamp\www\shcoyee/admin/view/template/');
-define('DIR_CONFIG', 'D:\wamp\www\shcoyee/system/config/');
-define('DIR_IMAGE', 'D:\wamp\www\shcoyee/image/');
-define('DIR_CACHE', 'D:\wamp\www\shcoyee/system/cache/');
-define('DIR_DOWNLOAD', 'D:\wamp\www\shcoyee/download/');
-define('DIR_LOGS', 'D:\wamp\www\shcoyee/system/logs/');
-define('DIR_CATALOG', 'D:\wamp\www\shcoyee/catalog/');
+define('DIR_APPLICATION', 'D:\www\shcoyee/mobile/admin/');
+define('DIR_SYSTEM', 'D:\www\shcoyee/mobile/system/');
+define('DIR_DATABASE', 'D:\www\shcoyee/mobile/system/database/');
+define('DIR_LANGUAGE', 'D:\www\shcoyee/mobile/admin/language/');
+define('DIR_TEMPLATE', 'D:\www\shcoyee/mobile/admin/view/template/');
+define('DIR_CONFIG', 'D:\www\shcoyee/mobile/system/config/');
+define('DIR_IMAGE', 'D:\www\shcoyee/mobile/image/');
+define('DIR_CACHE', 'D:\www\shcoyee/mobile/system/cache/');
+define('DIR_DOWNLOAD', 'D:\www\shcoyee/mobile/download/');
+define('DIR_LOGS', 'D:\www\shcoyee/mobile/system/logs/');
+define('DIR_CATALOG', 'D:\www\shcoyee/mobile/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysql');
